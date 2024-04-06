ecbcdabb852b708fc39e9c50bd98f0ce13033e7d
Draraedus <adriel2002santos@gmail.com>
2024-04-06 14:11:12 -0300

    character modified animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,64 +1,45 @@
-extends Area2D
+extends CharacterBody2D
 
-signal hit
+var _state_machine
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+@export_category("Variables")
+@export var _move_speed: float = 128.0
+@export var _friction: float = 0.8
+@export var _acceleration: float = 0.4
 
-# Called when the node enters the scene tree for the first time.
+@export_category("Objects")
+@export var _animation_tree: AnimationTree = null
+
 func _ready():
-	screen_size = get_viewport_rect().size
-	#hide()
+	_state_machine = _animation_tree["parameters/playback"]
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	# Player Movement
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
+func _physics_process(delta: float):
+	_move()
+	_animate()
+	move_and_slide()
 	
-	# Player not leave the screen  
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+func _move():
+	var _direction: Vector2 = Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	)
 	
-	if velocity.x < 0:
-		$AnimatedSprite2D.animation = "walk_right"
-		$AnimatedSprite2D.flip_v = false
-		# See the note below about the following boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.x > 0:
-		$AnimatedSprite2D.animation = "walk_left"
-		$AnimatedSprite2D.flip_v = false
-		# See the note below about the following boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x > 0
-	elif velocity.y > 0:
-		$AnimatedSprite2D.animation = "walk_up"
-		$AnimatedSprite2D.flip_v = velocity.y < 0
-	elif velocity.y < 0:
-		$AnimatedSprite2D.animation = "walk_down"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+	if _direction != Vector2.ZERO:
+		_animation_tree["parameters/idle/blend_position"] = _direction
+		_animation_tree["parameters/walk/blend_position"] = _direction
+		
+		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
+		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
+		return
+	
+	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
+	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
+	
+	velocity = _direction.normalized() * _move_speed
 
-
-func _on_body_entered(body):
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+func _animate():
+	if velocity.length() > 1:
+		_state_machine.travel("walk")
+		return
+		
+	_state_machine.travel("idle")
