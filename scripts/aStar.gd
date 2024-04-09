3a16bfe97db832db176c4a621d4ebe13a771ee29
Draraedus <adriel2002santos@gmail.com>
2024-04-09 17:30:32 -0300

    added aStar pathfinding

--- scripts/aStar.gd
+++ scripts/aStar.gd
@@ -1,3 +1,93 @@
 extends Node
 
 
+class NodeAux:
+	var position: Vector2
+	var parent: NodeAux = null
+	var g: float = 0
+	var h: float = 0
+	var f: float = 0
+
+	func _init(pos: Vector2, par: NodeAux = null):
+		position = pos
+		parent = par
+
+# A* algorithm that consider the map as a infinity plane cartesian coordinate system
+func a_star(start_pos: Vector2, end_pos: Vector2, is_diagonal: bool = false) -> Array:
+	var open_list = []
+	var closed_list = []
+
+	var start_node = NodeAux.new(start_pos)
+	var end_node = NodeAux.new(end_pos)
+
+	open_list.append(start_node)
+
+	while open_list.size() > 0:
+		var current_node = open_list[0]
+		var current_index = 0
+
+		for i in range(open_list.size()):
+			if open_list[i].f < current_node.f:
+				current_node = open_list[i]
+				current_index = i
+
+		open_list.remove_at(current_index)
+		closed_list.append(current_node)
+
+		if current_node.position.distance_to(end_node.position) < 6.0:
+			var path = []
+			while current_node != null:
+				path.append(current_node.position)
+				current_node = current_node.parent
+			path.reverse()
+			return path
+
+		var neighbors = get_neighbors(current_node.position, is_diagonal)
+
+		for neighbor in neighbors:
+			var in_closed = false
+			for closed_node in closed_list:
+				if closed_node.position == neighbor:
+					in_closed = true
+					break
+			if in_closed:
+				continue
+
+			var g_cost = current_node.g + 1
+			var h_cost = heuristic(neighbor, end_node.position)
+			var f_cost = g_cost + h_cost
+
+			var in_open = false
+			for open_node in open_list:
+				if open_node.position == neighbor and f_cost >= open_node.f:
+					in_open = true
+					break
+			if in_open:
+				continue
+
+			var neighbor_node = NodeAux.new(neighbor, current_node)
+			neighbor_node.g = g_cost
+			neighbor_node.h = h_cost
+			neighbor_node.f = f_cost
+
+			open_list.append(neighbor_node)
+
+	return []  # If path not encoutered
+
+func get_neighbors(position: Vector2, is_diagonal: bool) -> Array:
+	var neighbors = []
+	# horizontal and vertical neighbors
+	neighbors.append(position + Vector2(4, 0))
+	neighbors.append(position + Vector2(-4, 0))
+	neighbors.append(position + Vector2(0, 4))
+	neighbors.append(position + Vector2(0, -4))
+	# diagonal neighbors if required
+	if(is_diagonal):
+		neighbors.append(position + Vector2(4, 4))
+		neighbors.append(position + Vector2(-4, 4))
+		neighbors.append(position + Vector2(4, -4))
+		neighbors.append(position + Vector2(-4, -4))
+	return neighbors
+
+func heuristic(current: Vector2, goal: Vector2) -> float:
+	return abs(current.x - goal.x) + abs(current.y - goal.y)  # Manhattan Distance
--- scripts/hog_enemy.gd
+++ scripts/hog_enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var _state_machine
 
 @export_category("Variables")
-@export var _move_speed: float = 80.0
+@export var _move_speed: float = 100.0
 @export var _friction: float = 0.8
 @export var _acceleration: float = 0.4
 @export var target_path : Array
@@ -15,14 +15,18 @@ var _state_machine
 func _ready():
 	_state_machine = _animation_tree["parameters/playback"]
 
-func _process(delta: float):
+func _physics_process(delta: float):
 	_animate()
-	if !target_path.is_empty():
-		_move()
-		move_and_slide()
+	_move()
+	move_and_slide()
 	
 func _move():
-	var _direction: Vector2 = (Vector2(target_path[0]) - position.round()).normalized()
+	var _direction
+	if target_path.size() > 1:
+		_direction = (target_path[0] - Vector2(Vector2i(position))).normalized()
+		print(_direction)
+	else:
+		_direction = Vector2.ZERO
 	
 	if _direction != Vector2.ZERO:
 		_animation_tree["parameters/idle/blend_position"] = _direction
@@ -35,10 +39,9 @@ func _move():
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
 	
-	velocity = _direction.normalized() * _move_speed
 
 func _animate():
-	if velocity.length() > 1:
+	if velocity.length() > 2:
 		_state_machine.travel("walk")
 		return
 		
